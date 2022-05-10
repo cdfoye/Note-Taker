32ecb6400b46cc7522d5829465a7ac2fe1e7b92e
cdfoye <cdfoye@gmail.com>
2022-05-09 21:32:03 -0600

    fixed save button and created heroku app

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 
+//import modular  router for /apiroute
 const notesRouter = require('./apiroute');
 
 const app = express();
 
-app.use('/apiroute', notesRouter);
+app.use('/notes', notesRouter);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const path = require('path');
 
-const api = require('./routes/apiroute');
+const api = require('./routes/index.js');
 
 const app = express();
 
